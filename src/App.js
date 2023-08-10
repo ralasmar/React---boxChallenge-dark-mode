6e369daa5b9cb9d50913594fafb3d789e48f094e
Rola Alasmar <rola.alasmar@perficient.com>
2023-08-10 15:41:29 -0500

    box challenge dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import boxes from "./boxes"
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(props) {
+    //initialize state with the default value of the array pulled in from boxes.js
+    const [squares, setSquares] = React.useState(boxes)
+   
+    //creating a styles object allows it to be easily entered in for style css properties in a div
+    const styles = {
+         //use a ternary to change background color based on if darkMode is true or false
+        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+    }
+     //map over that state array and display each one as an empty square
+    const squareElements = squares.map(square => (
+        //can add a style property to the div and represent it as an object
+        <div style={styles} className="box" key={square.id}></div>
+    ))
+ 
+    return (
+        <main>
+            <h1>{squareElements}</h1>
+        </main>
+    )
 }
-
-export default App;
